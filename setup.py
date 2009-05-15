10371486baf45f2cfed49cf5a7c8eb924fd5af71
Karl Vollmer <vollmerk@vollmer.ampache.org>
2009-05-15 16:45:24 -0300

    Add licence file, desktop file icon and a setup.py from porthose desired by debian for packaging and useful to boot

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "quickplay",
+    version = "1.2",
+    description = "A light weight mp3 player and frontend for Ampache",
+    author = "Kevin Purdy",
+    author_email = "purdyk@onid.orst.edu",
+    url = "http://quickplay.ampache.org",
+    scripts = ["quickplay"],
+    long_description = "A light weight mp3 player/frontend for Ampache."
+    )
+      
